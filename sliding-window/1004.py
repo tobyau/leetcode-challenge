5e828ddf3456cb636fedb86100e4bcba17542d99
tobyau <tobyau9000@gmail.com>
2025-03-18 18:56:57 -0500

    Initial Commit

---
+++ sliding-window/1004.py
@@ -0,0 +1,16 @@
+'''
+1004. Max Consecutive Ones III
+
+Given a binary array nums and an integer k, return the maximum number of consecutive 1's in the array if you can flip at most k 0's.
+
+Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+Output: 6
+Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
+0 <= k <= nums.length
+'''
