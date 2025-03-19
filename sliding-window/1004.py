d61decf836eff4a74cbea9c7b72ed7027b4c9320
tobyau <tobyau9000@gmail.com>
2025-03-18 19:26:08 -0500

    1004 solution

--- sliding-window/1004.py
+++ sliding-window/1004.py
@@ -14,3 +14,29 @@ Constraints:
 nums[i] is either 0 or 1.
 0 <= k <= nums.length
 '''
+
+class Solution:
+  def longestOnes(self, nums: List[int], k: int) -> int:
+    '''
+    sliding window 
+    - keep track of zeros to maintain the window with k zeros 
+
+    Time: O(N)
+    Space: O(1) 
+    '''
+    ans = float('-inf')
+    left = 0 
+    zeros = 0 
+
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zeros += 1 
+        # increment left pointer to maintain window with k zeros 
+        while zeros > k:
+            if nums[left] == 0:
+                zeros -= 1 
+            left += 1 
+        
+        ans = max(ans, right - left + 1)
+    
+    return ans 
