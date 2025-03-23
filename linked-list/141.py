527b08f5ee14c5da5122b7a71af6eeaeb39af85d
tobyau <tobyau9000@gmail.com>
2025-03-23 13:46:43 -0500

    3/23

---
+++ linked-list/141.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        '''
+        detect cycle using slow fast pointer
+            - if fast pointer reaches slow pointer, there is a cycle 
+        '''
+        if not head:
+            return False
+        slow, fast = head, head.next 
+
+        while fast and fast.next: 
+            if fast == slow:
+                return True 
+            slow = slow.next 
+            fast = fast.next.next
+        
+        return False 
