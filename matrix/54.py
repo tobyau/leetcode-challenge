fd0d0bbb04e90db8c3decb2844415877583bde9f
tobyau <tobyau9000@gmail.com>
2025-04-17 22:08:45 -0500

    4/17

--- matrix/54.py
+++ matrix/54.py
@@ -1,28 +1,35 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        '''
+        Solution
+        - keep track of top, right, bottom, left indicies 
+
+        Time: O(m * n) 
+        Space: O(m * n) -> storing all elements in the matrix  
+        '''
         rows, cols = len(matrix), len(matrix[0])
-        left, right = 0, cols - 1 
         top, bottom = 0, rows - 1 
-        res = [] 
+        left, right = 0, cols - 1 
+        ans = []
 
         while top <= bottom and left <= right:
             # iterate top 
             for i in range(left, right + 1):
-                res.append(matrix[top][i])
+                ans.append(matrix[top][i]) 
             top += 1 
             # iterate right 
             for i in range(top, bottom + 1):
-                res.append(matrix[i][right])
+                ans.append(matrix[i][right]) 
             right -= 1 
-            # iterate bottom in reverse 
+            # iterate bottom 
             if top <= bottom:
                 for i in range(right, left - 1, -1):
-                    res.append(matrix[bottom][i])
+                    ans.append(matrix[bottom][i])
                 bottom -= 1 
-            # iterate left to right 
+            # iterate left 
             if left <= right:
                 for i in range(bottom, top - 1, -1):
-                    res.append(matrix[i][left])
+                    ans.append(matrix[i][left])
                 left += 1 
         
-        return res 
+        return ans 
--- misc/9.py
+++ misc/9.py
@@ -1,12 +1,17 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        str_x = str(x) 
-        left, right = 0, len(str_x) - 1 
+        '''
+        turn into string and use 2 pointer 
+        Time: O(N)
+        Space: O(1) 
+        '''
+        x = str(x) 
+        left, right = 0, len(x) - 1 
 
-        while left <= right:
-            if str_x[left] != str_x[right]:
+        while left < right:
+            if x[left] != x[right]:
                 return False 
             left += 1 
             right -= 1 
-            
-        return True 
+        
+        return True  
